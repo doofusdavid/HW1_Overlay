32b66138dbd866e5d0691ff3303dad90184b63a5
David Edwards <david.edwards@colostate.edu>
2017-01-28 11:22:27 -0700

    Code Deregister wireformat

--- src/cs455/overlay/wireformats/Deregister.java
+++ src/cs455/overlay/wireformats/Deregister.java
@@ -1,8 +1,62 @@
 package cs455.overlay.wireformats;
 
+import java.io.*;
+
 /**
  *  Deregister - When a node tears down, it must deregister itself from the Registry using this format
  */
-public class Deregister
+public class Deregister implements Event
 {
+    public int type;
+    public String IPAddress;
+    public int Port;
+
+    @Override
+    public int getType()
+    {
+        return 0;
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException
+    {
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        dout.writeInt(this.type);
+
+        byte[] ipBytes = IPAddress.getBytes();
+        int ipLength = ipBytes.length;
+        dout.writeInt(ipLength);
+        dout.write(ipBytes);
+
+        dout.writeInt(this.Port);
+
+        dout.flush();
+        marshalledBytes = baOutputStream.toByteArray();
+        baOutputStream.close();
+        dout.close();
+
+        return marshalledBytes;
+    }
+
+    public Deregister(byte[] marshalledBytes) throws IOException
+    {
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+
+        this.type = din.readInt();
+
+        int ipLength = din.readInt();
+        byte[] ipBytes = new byte[ipLength];
+        din.readFully(ipBytes);
+
+        this.IPAddress = new String(ipBytes);
+
+        this.Port = din.readInt();
+
+        baInputStream.close();
+        din.close();
+    }
 }
--- src/cs455/overlay/wireformats/Event.java
+++ src/cs455/overlay/wireformats/Event.java
@@ -1,10 +1,26 @@
 package cs455.overlay.wireformats;
 
+import java.io.IOException;
+
 /**
  * Created by david on 1/21/17.
  */
 public interface Event
 {
-     int getType();
-     byte[] getBytes();
+    public static enum MessageType
+    {
+        REGISTER_REQUEST,
+        REGISTER_RESPONSE,
+        DEREGISTER_REQUEST,
+        MESSAGING_NODES_LIST,
+        LINK_WEIGHTS,
+        TASK_INITIATE,
+        TASK_COMPLETE,
+        PULL_TRAFFIC_SUMMARY,
+        TRAFFIC_SUMMARY
+
+    }
+    int getType();
+    byte[] getBytes() throws IOException;
+
 }
