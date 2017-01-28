6d092d1fceda1cf99654971344c8751b25392c1c
David Edwards <david.edwards@colostate.edu>
2017-01-28 13:11:47 -0700

    First stab at setting up TCP Receiver

--- src/cs455/overlay/node/MessagingNode.java
+++ src/cs455/overlay/node/MessagingNode.java
@@ -3,6 +3,11 @@ package cs455.overlay.node;
 import cs455.overlay.transport.TCPReceiverThread;
 import cs455.overlay.transport.TCPSender;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by david on 1/21/17.
  */
@@ -10,7 +15,8 @@ public class MessagingNode implements Node
 {
     private TCPReceiverThread receiver;
     private TCPSender sender;
-
+    private int receivedMessageCount;
+    private int sentMessageCount;
 
     @Override
     public void onEvent()
@@ -19,9 +25,36 @@ public class MessagingNode implements Node
 
     public MessagingNode()
     {
+
+        try (Socket socket = new Socket(InetAddress.getLocalHost(), 1000))
+        {
+            receiver = new TCPReceiverThread(socket);
+            receiver.run();
+        }
+        catch (UnknownHostException ue)
+        {
+            System.out.println(ue.getMessage());
+        }
+        catch (IOException ie)
+        {
+            System.out.println(ie.getMessage());
+        }
+
         // Connect to Registry
         // Register self
 
 
+    }
+
+    /**
+     * Create this as synchronized so that two threads can't update the counter simultaneously.
+     */
+    private synchronized void incrementSentCounter()
+    {
+        this.sentMessageCount++;
+    }
+    private synchronized void incrementReceivedCounter()
+    {
+        this.receivedMessageCount++;
     }
 }
