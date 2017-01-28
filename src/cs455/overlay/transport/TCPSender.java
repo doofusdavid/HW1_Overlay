dd62ebfce14efed4f581e65b42c909161557e2cb
David Edwards <david.edwards@colostate.edu>
2017-01-28 11:37:49 -0700

    Code TCP Sender

--- src/cs455/overlay/transport/TCPSender.java
+++ src/cs455/overlay/transport/TCPSender.java
@@ -1,8 +1,30 @@
 package cs455.overlay.transport;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by david on 1/21/17.
  */
 public class TCPSender
 {
+    private Socket socket;
+    private DataOutputStream dout;
+
+    public TCPSender(Socket socket) throws IOException
+    {
+        this.socket = socket;
+        dout = new DataOutputStream(socket.getOutputStream());
+    }
+
+    public void sendData(byte[] dataToSend) throws IOException
+    {
+        int dataLength = dataToSend.length;
+        dout.writeInt(dataLength);
+
+        dout.write(dataToSend,0,dataLength);
+        dout.flush();
+
+    }
 }
