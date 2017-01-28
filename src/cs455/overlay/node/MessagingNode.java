4b67559eb5e345b5f53366315338b2455a1f1c0f
David Edwards <david.edwards@colostate.edu>
2017-01-28 12:43:09 -0700

    Sort out Node Stub out Messaging Node and Registry Remove MesssageType enum, as that's going to be a separate class for now.

--- src/cs455/overlay/node/MessagingNode.java
+++ src/cs455/overlay/node/MessagingNode.java
@@ -1,12 +1,27 @@
 package cs455.overlay.node;
 
+import cs455.overlay.transport.TCPReceiverThread;
+import cs455.overlay.transport.TCPSender;
+
 /**
  * Created by david on 1/21/17.
  */
 public class MessagingNode implements Node
 {
+    private TCPReceiverThread receiver;
+    private TCPSender sender;
+
+
     @Override
     public void onEvent()
     {
+    }
+
+    public MessagingNode()
+    {
+        // Connect to Registry
+        // Register self
+
+
     }
 }
--- src/cs455/overlay/node/Registry.java
+++ src/cs455/overlay/node/Registry.java
@@ -1,10 +1,16 @@
 package cs455.overlay.node;
 
+import cs455.overlay.transport.TCPReceiverThread;
+import cs455.overlay.transport.TCPSender;
+
 /**
  * Created by david on 1/21/17.
  */
 public class Registry implements Node
 {
+    private TCPReceiverThread receiver;
+    private TCPSender sender;
+
     @Override
     public void onEvent()
     {
