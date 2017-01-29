a046433bb778389138a9c581735edb1fa6658575
David Edwards <david.edwards@colostate.edu>
2017-01-29 14:47:08 -0700

    Need StatusCode for RegisterResponse

---
+++ src/cs455/overlay/wireformats/StatusCode.java
@@ -0,0 +1,10 @@
+package cs455.overlay.wireformats;
+
+/**
+ * Created by david on 1/29/17.
+ */
+public class StatusCode
+{
+    public static final byte SUCCESS = (byte)1;
+    public static final byte FAILURE = (byte)0;
+}
