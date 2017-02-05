3b01cb7605143b4f45b51f94c0bd1100a9ebb912
David Edwards <david.edwards@colostate.edu>
2017-02-05 14:10:47 -0700

    Create TaskInitiate message

--- src/cs455/overlay/wireformats/TaskInitiate.java
+++ src/cs455/overlay/wireformats/TaskInitiate.java
@@ -1,9 +1,55 @@
 package cs455.overlay.wireformats;
 
+import java.io.*;
+
 /**
  * The registry informs nodes in the overlay when they should start sending messages to each other
  * using TaskInitiate
  */
-public class TaskInitiate
+public class TaskInitiate implements Event
 {
+    public final int type = MessageType.TASK_INITIATE;
+    public int rounds;
+
+
+    @Override
+    public int getType()
+    {
+        return this.type;
+    }
+
+    public TaskInitiate(byte[] marshalledBytes) throws IOException
+    {
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+
+        int type = din.readInt();
+        if (type != MessageType.TASK_INITIATE)
+        {
+            throw new IllegalArgumentException("Bytes didn't correspond to TaskInitiate");
+        }
+
+        this.rounds = din.readInt();
+
+        baInputStream.close();
+        din.close();
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException
+    {
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        dout.writeInt(this.type);
+        dout.writeInt(this.rounds);
+
+        dout.flush();
+        marshalledBytes = baOutputStream.toByteArray();
+        baOutputStream.close();
+        dout.close();
+
+        return marshalledBytes;
+    }
 }
