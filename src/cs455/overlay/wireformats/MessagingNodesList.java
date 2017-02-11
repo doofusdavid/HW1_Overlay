8b8402ad5b8468257acd2ec92c58b78e9066e2ae
David Edwards <david.edwards@colostate.edu>
2017-02-11 16:43:29 -0700

    create MessagingNodesList

--- src/cs455/overlay/wireformats/MessagingNodesList.java
+++ src/cs455/overlay/wireformats/MessagingNodesList.java
@@ -1,34 +1,85 @@
 package cs455.overlay.wireformats;
 
-import cs455.overlay.util.NotImplementedException;
+import cs455.overlay.dijkstra.NodeDescriptor;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 
 public class MessagingNodesList implements Event
 {
-    private int messageType;
+    private int type = MessageType.MESSAGING_NODES_LIST;
+    private int numberOfPeers;
+    private ArrayList<NodeDescriptor> neighborNodes;
 
-    public MessagingNodesList(byte[] data)
+    public MessagingNodesList(int numberOfPeers, ArrayList<NodeDescriptor> neighborNodes)
     {
-        this();
-        throw new NotImplementedException();
+        this.numberOfPeers = numberOfPeers;
+        this.neighborNodes = neighborNodes;
+    }
+
+    public MessagingNodesList(byte[] marshalledBytes) throws IOException
+    {
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+
+        int type = din.readInt();
+        if (type != MessageType.MESSAGING_NODES_LIST)
+        {
+            throw new IllegalArgumentException("Bytes didn't correspond to a MessagingNodeList.");
+        }
+        this.numberOfPeers = din.readInt();
+        this.neighborNodes = new ArrayList<>(this.numberOfPeers);
+
+        for (int i = 0; i < this.numberOfPeers; i++)
+        {
+            int nodeLength = din.readInt();
+            byte[] nodeBytes = new byte[nodeLength];
+            din.readFully(nodeBytes);
+            this.neighborNodes.add(new NodeDescriptor(nodeBytes));
+        }
+
+        baInputStream.close();
+        din.close();
+
     }
 
     public MessagingNodesList()
     {
-        this.messageType = MessageType.LINK_WEIGHTS;
+
     }
 
     @Override
     public int getType()
     {
-        return 0;
+        return this.type;
     }
 
     @Override
     public byte[] getBytes() throws IOException
     {
-        return new byte[0];
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        dout.writeInt(this.type);
+
+        dout.writeInt(this.numberOfPeers);
+
+        for (NodeDescriptor node : this.neighborNodes)
+        {
+            String nodeString = node.toString();
+            byte[] nodeBytes = nodeString.getBytes();
+            int nodeLength = nodeBytes.length;
+            dout.writeInt(nodeLength);
+            dout.write(nodeBytes);
+        }
+
+        dout.flush();
+        marshalledBytes = baOutputStream.toByteArray();
+        baOutputStream.close();
+        dout.close();
+
+        return marshalledBytes;
     }
 }
